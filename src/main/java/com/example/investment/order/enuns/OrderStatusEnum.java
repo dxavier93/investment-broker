0d5038f203ffe3b0a1902ee203e3a32580afbe97
dxavier <deorgenes.xavier@capgemini.com>
2023-03-01 14:33:00 +0000

    Update to synch kafka responses and join 2 topics to the final response

--- src/main/java/com/example/investment/order/enuns/OrderStatusEnum.java
+++ src/main/java/com/example/investment/order/enuns/OrderStatusEnum.java
@@ -2,11 +2,14 @@ package com.example.investment.order.enuns;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
 public enum OrderStatusEnum {
   COMPLETE,
+  ACCOUNT_OK,
+  STOCK_OK,
   PENDING,
   FAIL
 }
--- src/main/java/com/example/investment/order/messaging/consumer/OrderConsumer.java
+++ src/main/java/com/example/investment/order/messaging/consumer/OrderConsumer.java
@@ -6,7 +6,6 @@ import com.example.investment.order.model.Order;
 import com.example.investment.order.repository.OrderRepository;
 import com.example.investment.stock.model.StockResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
@@ -14,48 +13,75 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static com.example.investment.order.enuns.OrderStatusEnum.COMPLETE;
 import static com.example.investment.order.enuns.OrderStatusEnum.FAIL;
 
 @Component
 @Slf4j
-@AllArgsConstructor
 public class OrderConsumer {
   private final OrderRepository repository;
   private final ObjectMapper objectMapper;
 
+  private Queue<String> globalQueue;
+
+  public OrderConsumer(OrderRepository repository, ObjectMapper objectMapper) {
+    this.repository = repository;
+    this.objectMapper = objectMapper;
+    this.globalQueue = new ConcurrentLinkedQueue<>();
+  }
+
   @KafkaListener(topics = "#{'${spring.kafka.account.response.topic.name}'}", groupId = "1")
   public void onReceiveAccount(ConsumerRecord event) throws IOException {
-    OrderStatusEnum status = FAIL;
     AccountResponse response =
         objectMapper.readValue(event.value().toString(), AccountResponse.class);
 
-    log.info(String.format("Received Account Message: [%s]", response));
-
-    Order order = repository.findById(response.getStockOrderId()).orElseThrow(
-        () -> new ResourceNotFoundException("No stock order was found by this id.")
-    );
-
-    if (response.getStatus().equals("OK")) {
-      status = COMPLETE;
+    synchronized (this) {
+      checkOrderStatus(response.getStatus(), response.getStockOrderId());
     }
-
-    log.info(String.format("OPERATION %s", status));
-    order.setStatus(status);
-    repository.save(order);
   }
 
   @KafkaListener(topics = "#{'${spring.kafka.stock.response.topic.name}'}", groupId = "1")
   public void onReceiveStock(ConsumerRecord event) throws IOException {
-    OrderStatusEnum status = FAIL;
     StockResponse response =
         objectMapper.readValue(event.value().toString(), StockResponse.class);
 
     log.info(String.format("Received Stock Message: [%s]", response));
 
-    Order order = repository.findById(response.getStockOrderId()).orElseThrow(
-        () -> new ResourceNotFoundException("No Order was found by this id.")
-    );
+    synchronized (this) {
+      checkOrderStatus(response.getStatus(), response.getStockOrderId());
+    }
+  }
+
+  private void checkOrderStatus(String orderStatus, long id) {
+    OrderStatusEnum finalStatus = FAIL;
+
+    synchronized (this) {
+
+      globalQueue.add(orderStatus);
+
+      log.info("Responses: " + globalQueue);
+
+      if (globalQueue.size() > 1) {
+
+        Order order = repository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("No stock order was found by this id.")
+        );
+
+        if (!order.getStatus().equals(COMPLETE)) {
+
+          if (globalQueue.stream().filter(it -> it.equals("OK")).count() == 2) {
+            finalStatus = COMPLETE;
+            globalQueue.clear();
+          }
+
+          order.setStatus(finalStatus);
+          log.info(String.format("OPERATION %s", order.getStatus()));
+          repository.save(order);
+        }
+      }
+    }
   }
 }
